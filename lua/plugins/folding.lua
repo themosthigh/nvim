fc15b2c4ac1d1939b0ec4838202559f8c66060e1
TheMostHigh <tallestlegacy@gmail.com>
2025-03-11 12:16:33 +0300

    fold: fail at improving the visual folding experience

--- lua/plugins/folding.lua
+++ lua/plugins/folding.lua
@@ -34,6 +34,14 @@ return {
     "kevinhwang91/nvim-ufo",
     dependencies = { 'kevinhwang91/promise-async' },
     config = function()
+      -- FOLDING
+      vim.o.foldcolumn = '0' -- '0' is not bad
+      vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+      -- vim.o.fillchars = 'eob: ,fold: ,foldopen:˅,foldsep:|,foldclose:›'
+
+
       require('ufo').setup({
         provider_selector = function(bufnr, filetype, buftype)
           return { 'lsp', 'indent' }
